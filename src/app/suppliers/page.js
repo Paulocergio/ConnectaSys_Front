721b47f27c62d329655cf8322b65dd8109c845db
Paulo Cergio <juniorcergio@gmail.com>
2025-06-04 21:43:33 -0300

    feat: adicionar nova rota e ícone para a seção de fornecedores no sidebar

---
+++ src/app/suppliers/page.js
@@ -0,0 +1,26 @@
+"use client";
+import { useState } from "react";
+import Sidebar from "../../components/Sidebar";
+import Table from "../../components/Table/Table";
+export default function Suppliers() {
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
+
+    const toggleSidebar = () => setIsSidebarCollapsed((prev) => !prev);
+
+    return (
+        <div className="flex h-screen">
+            <Sidebar isCollapsed={isSidebarCollapsed} toggleSidebar={toggleSidebar} />
+            <main className="flex-1 p-6 bg-gray-50 overflow-auto">
+                <Table
+                    title="Fornercedores"
+                    data={[]} // Aqui você deve passar os dados dos fornecedores
+
+
+
+
+                />
+            </main>
+          
+        </div>
+    );
+}
