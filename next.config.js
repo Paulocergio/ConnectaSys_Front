3a4fec60bdb9ea4a7acd9f78409962dea0ace2e7
Paulo Cergio <juniorcergio@gmail.com>
2025-08-05 13:12:35 -0300

    Refactor user and supplier service API endpoints for consistency and clarity

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ⬅️ isso substitui o next export
+  output: 'export',
+  images: {
+    unoptimized: true, 
+  },
 }
 
 module.exports = nextConfig
